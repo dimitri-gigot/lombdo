9e328ffcd5017b9a3731f3faf7173f95f06a2353
dimitri-gigot <dimgigot@gmail.com>
2017-08-24 10:25:34 +0200

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+let cachedDb = null;
+
+module.exports = (DB_STRING) => (lombdo) => (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false
+
+    connectToDatabase(DB_STRING).then(() => {
+      lombdo(
+        {event, context},
+        {json, html},
+        {event, context, callback}
+      )
+    })
+
+   function error(status, message, error) {
+     json(status || 500, data || {
+       message: 'unexpected error',
+       error: error
+     })
+   }
+
+   function json(status, data){
+     callback(null, {
+       statusCode: status,
+       headers: {
+         'Content-type': 'application/json; charset=utf-8'
+       },
+       body: JSON.stringify({data : data})
+     })
+   }
+
+   function html(status, str){
+     callback(null, {
+       statusCode: status,
+       headers: {
+         'Content-type': 'text/html; charset=utf-8'
+       },
+       body: str
+     })
+   }
+}
+
+function connectToDatabase(uri) {
+  if (cachedDb && cachedDb.serverConfig.isConnected())return Promise.resolve(cachedDb)
+  return mongoose.connect(uri).then(db => { cachedDb = db; return cachedDb; })
+}
